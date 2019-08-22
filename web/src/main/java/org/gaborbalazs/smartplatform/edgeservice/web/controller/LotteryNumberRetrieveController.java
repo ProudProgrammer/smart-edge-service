c8f64bc1f1c352cb2d395b94dc08dc0ae87106e4
Balázs Gábor <mail.gabor.balazs@gmail.com>
2019-08-22 23:40:16 +0200

    Separate Swagger and Spring annotations with different interfaces

--- web/src/main/java/org/gaborbalazs/smartplatform/edgeservice/web/controller/LotteryNumberRetrieveController.java
+++ web/src/main/java/org/gaborbalazs/smartplatform/edgeservice/web/controller/LotteryNumberRetrieveController.java
@@ -1,18 +1,18 @@
 package org.gaborbalazs.smartplatform.edgeservice.web.controller;
 
-import java.util.Collections;
 import java.util.Set;
 
 import org.gaborbalazs.smartplatform.edgeservice.service.enums.LotteryType;
 import org.gaborbalazs.smartplatform.edgeservice.service.retrieve.LotteryNumberRetrieveService;
 import org.gaborbalazs.smartplatform.edgeservice.web.api.LotteryNumberRetrieveApi;
+import org.gaborbalazs.smartplatform.edgeservice.web.api.LotteryNumberRetrieveSwaggerApi;
 import org.gaborbalazs.smartplatform.edgeservice.web.converter.LotteryTypeConverter;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class LotteryNumberRetrieveController implements LotteryNumberRetrieveApi {
+class LotteryNumberRetrieveController implements LotteryNumberRetrieveApi, LotteryNumberRetrieveSwaggerApi {
 
     private LotteryNumberRetrieveService lotteryNumberRetrieveService;
 
@@ -20,6 +20,7 @@ class LotteryNumberRetrieveController implements LotteryNumberRetrieveApi {
         this.lotteryNumberRetrieveService = lotteryNumberRetrieveService;
     }
 
+    @Override
     public Set<Integer> retrieveRandom(LotteryType lotteryType) {
         return lotteryNumberRetrieveService.retrieveRandom(lotteryType);
     }
